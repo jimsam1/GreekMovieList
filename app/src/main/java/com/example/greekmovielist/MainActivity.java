14665cb970dbf3b6bd1110443ba6ebad1061c1fc
jimsam1 <dimitris.samras@gmail.com>
2022-04-04 21:30:53 +0300

    added next page button (intent)

--- app/src/main/java/com/example/greekmovielist/MainActivity.java
+++ app/src/main/java/com/example/greekmovielist/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.greekmovielist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Button nextPageButton;
+
+    public void nextPage (View view){
+        Intent i = new Intent(this, MainActivity2.class);
+//Pass data to the SayHelloNewScreen Activity through the Intent
+        nextPageButton = findViewById(R.id.nextPageBtn);
+//Ask Android to start the new Activity
+        startActivity(i);
+    }
+
+
 }
