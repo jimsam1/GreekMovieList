3c903746e9b51b5aad9bf036113a97509798a31c
jimsam1 <dimitris.samras@gmail.com>
2022-04-12 19:07:31 +0300

    Added DataBase Helper Added Movie Class Updated MainActivity Layout with add movie components

--- app/src/main/java/com/example/greekmovielist/MainActivity.java
+++ app/src/main/java/com/example/greekmovielist/MainActivity.java
@@ -6,24 +6,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button nextPageButton, addMovieButton;
+    EditText addName, addPlot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    Button nextPageButton;
+        nextPageButton = findViewById(R.id.nextPageBtn);
+        addMovieButton = findViewById(R.id.addNewMovie);
+        addName = findViewById(R.id.addName);
+        addPlot = findViewById(R.id.addPlot);
+    }
 
     public void nextPage (View view){
         Intent i = new Intent(this, MainActivity2.class);
-//Pass data to the SayHelloNewScreen Activity through the Intent
-        nextPageButton = findViewById(R.id.nextPageBtn);
-//Ask Android to start the new Activity
+        //Pass data to the SayHelloNewScreen Activity through the Intent
+
+        //Ask Android to start the new Activity
         startActivity(i);
     }
+
+    public void addMovie (View view){
+        Movie movie;
+        try {
+            movie = new Movie(-1, addName.getText().toString(),addPlot.getText().toString());
+            Toast.makeText(MainActivity.this, movie.toString(), Toast.LENGTH_SHORT).show();
+        }catch(Exception e){
+            Toast.makeText(MainActivity.this, "error creating movie", Toast.LENGTH_SHORT).show();
+            movie = new Movie(-1, "error", "error");
+        }
+
+        DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
+
+        boolean success = dataBaseHelper.addOne(movie);
+
+        Toast.makeText(MainActivity.this, "Success= " + success, Toast.LENGTH_SHORT).show();
+
+    }
+
+
 
 
 }
