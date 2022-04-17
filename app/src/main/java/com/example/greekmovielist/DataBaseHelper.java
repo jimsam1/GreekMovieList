0fcd83c24528d635feff1adfd676403e20b1c644
jimsam1 <dimitris.samras@gmail.com>
2022-04-17 18:59:02 +0300

    Added Select All from DB (changes in DataBaseHelper,MainActivity2)

--- app/src/main/java/com/example/greekmovielist/DataBaseHelper.java
+++ app/src/main/java/com/example/greekmovielist/DataBaseHelper.java
@@ -2,10 +2,14 @@ package com.example.greekmovielist;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
@@ -51,5 +55,34 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         else {
             return true;
         }
+    }
+
+    public List<Movie> getAllMovies() {
+
+        List<Movie> returnList = new ArrayList<>();
+
+        //κάνουμε select από τη βάση
+        String query = "Select * FROM " + MOVIE_TABLE;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+        //αν έχουμε true σημαίνει ότι επιστράφηκαν αποτελέσματα
+        if (cursor.moveToFirst()){
+            do {
+                int movieID = cursor.getInt(0); //έβαλα μηδέν γιατί ξέρω ότι το id είναι στη θέση μηδέν
+                String movieName = cursor.getString(1);
+                String moviePlot = cursor.getString(2);
+
+                Movie newMovie = new Movie(movieID,movieName,moviePlot);
+                returnList.add(newMovie);
+            }while(cursor.moveToNext()); //παίρνει ένα-ένα τα στοιχεία της βάσης
+        }
+        else{
+            //επειδή δε θα έχει επιστραφεί κάτι από τη βάση, δε θα εισάγουμε τίποτα στη λίστα
+        }
+        cursor.close();
+        db.close();
+
+        return returnList;
     }
 }
--- app/src/main/java/com/example/greekmovielist/MainActivity2.java
+++ app/src/main/java/com/example/greekmovielist/MainActivity2.java
@@ -3,12 +3,33 @@ package com.example.greekmovielist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    ListView lv_movieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        lv_movieList = findViewById(R.id.lv_movieList);
+    }
+
+    public void viewAll(View view) {
+
+        DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity2.this);
+        List<Movie> allMovies = dataBaseHelper.getAllMovies();
+
+        ArrayAdapter movieArrayAdapter = new ArrayAdapter<Movie>(MainActivity2.this, android.R.layout.simple_list_item_1, allMovies);
+        lv_movieList.setAdapter(movieArrayAdapter);
+
+        //Toast.makeText(MainActivity2.this, allMovies.toString(), Toast.LENGTH_SHORT).show();
     }
 }
