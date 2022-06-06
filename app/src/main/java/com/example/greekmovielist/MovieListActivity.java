3a1ec9f62acdc63c2a11932dd8546d77d86265e2
jimsam1 <dimitris.samras@gmail.com>
2022-06-06 23:56:14 +0300

    renamed file

--- app/src/main/java/com/example/greekmovielist/MovieListActivity.java
+++ app/src/main/java/com/example/greekmovielist/MovieListActivity.java
@@ -17,7 +17,7 @@ public class MovieListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_movie_list);
 
         recyclerView = findViewById(R.id.recycler_view);
 
